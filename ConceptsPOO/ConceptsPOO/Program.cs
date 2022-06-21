1d652a5581bf491bc02cf9d492ef8771c0e366ee
AngelLogue <miguel.loera@daltum.mx>
2022-06-21 14:34:17 -0500

    Finish day class

---
+++ ConceptsPOO/ConceptsPOO/Program.cs
@@ -0,0 +1,15 @@
+﻿using ConceptsPOO;
+
+Console.WriteLine("Hello, World!");
+
+try
+{
+    Console.WriteLine(new Date(2000, 10, 7));
+    Console.WriteLine(new Date(1974, 9, 23));
+    Console.WriteLine(new Date(2024, 11, 31));
+}
+catch (Exception error)
+{
+
+    Console.Write(error.Message);
+}
